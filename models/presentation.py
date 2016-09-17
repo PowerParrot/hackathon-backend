21f23fa3dbde5111a208eba88bd61b356e4d322e
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-17 02:53:05 +0200

    Add set_current_page and json dumps

--- models/presentation.py
+++ models/presentation.py
@@ -1,4 +1,5 @@
 import os
+from bson.json_util import dumps
 from werkzeug.utils import secure_filename
 
 
@@ -11,18 +12,15 @@ class PresentationCollection:
     def add_presentation(self, pdf_file):
         # save empty presentation without file
         result = self.mongo.db.presentations.insert_one({'originalFilename': secure_filename(pdf_file.filename)})
-
         # get id of empty record as filename
         filename = str(result.inserted_id)
-
         absolute_path = os.path.join(self.app.root_path + self.app.config['UPLOAD_FOLDER'], filename)
-
         # save file with id as filename
         pdf_file.save(absolute_path)
-
-        print(result.inserted_id)
-
         # update record with path of file
         self.mongo.db.presentations.update_one({'_id': result.inserted_id}, {'$set': {'pdfPath': absolute_path}})
+        return dumps(self.mongo.db.presentations.find_one({'_id': result.inserted_id}))
 
-        return result.inserted_id
+    def set_current_page(self, presentation_id, current_page):
+        self.mongo.db.presentations.update_one({'_id': presentation_id}, {'$set': {'currentPage': current_page}})
+        return dumps(self.mongo.db.presentations.find_one({'_id':presentation_id}))
