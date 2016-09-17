c7a0927d7731b1d806d7877a42ec07047e32bd2d
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-17 04:40:01 +0200

    Shit.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from models.presentation import PresentationCollection
+import socketio
+import eventlet
+
 
 UPLOAD_FOLDER = '/pdfs'
 
 app = Flask('hackathon')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+sio = socketio.Server()
 
 mongo = PyMongo(app)
 
@@ -18,3 +22,14 @@ def upload_file():
     return 'file uploaded successfully'
 
 
+@sio.on('audio')
+def handlechunk(sid, data):
+    print(data)
+
+
+if __name__ == '__main__':
+    # wrap Flask application with socketio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
