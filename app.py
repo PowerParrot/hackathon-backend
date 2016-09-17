b6b8583db916e4c5a355344ea2370558abdf868d
Dominik Schöni <dominik@dominikschoeni.ch>
2016-09-17 04:52:09 +0200

    swag

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-
+from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
-from models.presentation import PresentationCollection
-import socketio
-import eventlet
 
+from models.presentation import PresentationCollection
 
 UPLOAD_FOLDER = '/pdfs'
 
 app = Flask('hackathon')
 CORS(app)
 
+# socket.io
+socketio = SocketIO(app)
+if __name__ == '__main__':
+    socketio.run(app)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-sio = socketio.Server()
 
 mongo = PyMongo(app)
-
 presentation = PresentationCollection(mongo, app)
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -30,19 +30,6 @@ def change_page():
     updated_presentation = presentation.set_current_page(request.args.get('presentation_id'), request.args.get('current_page'))
     return updated_presentation
 
-
-@sio.on('audio')
-def handlechunk(sid, data):
+@socketio.on('audio')
+def handlechunk(data):
     print(data)
-
-
-if __name__ == '__main__':
-    # wrap Flask application with socketio's middleware
-    app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
-
-
-
-
