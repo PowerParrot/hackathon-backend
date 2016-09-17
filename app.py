f46e9edd083a45cd8d33677a11eb175c82de1bf8
Dominik Schöni <dominik@dominikschoeni.ch>
2016-09-17 03:14:12 +0200

    adding CORS

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 from flask_pymongo import PyMongo
 from models.presentation import PresentationCollection
 
 UPLOAD_FOLDER = '/pdfs'
 
 app = Flask('hackathon')
+CORS(app)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mongo = PyMongo(app)
@@ -16,4 +20,3 @@ presentation = PresentationCollection(mongo, app)
 def upload_file():
     created_presentation = presentation.add_presentation(request.files['file'])
     return created_presentation
-
