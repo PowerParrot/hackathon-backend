e8d997c1f95c3640e2a94552ed5b9cf2a8ecace5
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-17 17:10:33 +0200

    Return json object instead of string.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, url_for
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
+import json
 
 from models.presentation import PresentationCollection
 
-from services.magic import add_chunk, main
 
 UPLOAD_FOLDER = '/static/pdfs'
 
@@ -36,7 +36,8 @@ def change_page():
 @app.route('/getDocumentPath', methods=['GET'])
 def get_file_url():
     object_id = request.args.get('presentation_id')
-    return url_for('static', filename='pdfs/' + object_id)
+    json_object = {'url': url_for('static', filename='pdfs/' + object_id)}
+    return json.dumps(json_object)
 
 if __name__ == '__main__':
     socketio.run(app)
