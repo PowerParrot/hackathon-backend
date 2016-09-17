561a7772b954263a96ceb2f6d6971023b097be8a
Genc Mazlami <genc.mazlami@uzh.ch>
2016-09-17 17:05:02 +0200

    Add translationEngine

---
+++ services/translate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Simple command-line example for Translate.
+
+Command-line application that translates some text.
+"""
+from __future__ import print_function
+from googleapiclient.discovery import build
+import os
+
+
+class TranslateEngine:
+
+    def __init__(self):
+        dev_key = os.environ['GOOGLE_DEV_KEY']
+        self.service = build('translate', 'v2', developerKey=dev_key)
+
+    def translate(self, sentence_list, language):
+        return self.service.translations().list(source='en', target=language, q=sentence_list).execute()
+
+
